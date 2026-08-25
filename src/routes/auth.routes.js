e2a870d2e84b9a9f523855bd91c04c0a1cdd0bd7
mpho molefe <molefempho783@gmail.com>
2026-08-25 15:03:56 +0200

    implement JWT-based authentication

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,9 +1,11 @@
 const express = require("express");
-const { registerUser, loginUser } = require("../controllers/auth.controller");
+const { registerUser, loginUser, getCurrentUser } = require("../controllers/auth.controller");
+const { authenticate } = require("../middleware/authenticate");
 
 const router = express.Router();
 
 router.post("/register", registerUser);
 router.post("/login", loginUser);
+router.get("/me", authenticate, getCurrentUser);
 
 module.exports = router;
--- src/server.js
+++ src/server.js
@@ -1,3 +1,5 @@
+require("dotenv").config();
+
 const { createApp } = require("./app");
 
 function startServer() {
